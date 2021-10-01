73574009c233feea3dbc808b8aa245901b9e8bed
framzik <framzik@gmail.com>
2021-10-01 17:23:07 +0300

    init sql

---
+++ src/main/resources/init.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS public.fias_address
+(
+    id            bigserial NOT NULL,
+    objectid      bigint NOT NULL,
+    name          character varying(250),
+    typename      character varying(50),
+    level         character varying(250),
+    oktmo         character varying(11),
+    objectguid    character varying(36),
+    isactive      boolean,
+    parentobjid   bigint,
+    apart_number  character varying(50),
+    aparttype     integer,
+    housenum      character varying(50),
+    addnum1       character varying(50),
+    addnum2       character varying(50),
+    housetype     integer,
+    addtype1      integer,
+    addtype2      integer,
+    steads_number character varying(250)
+)
+    TABLESPACE pg_default;
+
+ALTER TABLE public.fias_address
+    OWNER to fiz;
